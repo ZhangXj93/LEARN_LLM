bfd46095f34a18abc0d7e3b2e44575a40c72197f
Jagger Zhang <xjzhang93@163.com>
2023-11-05 15:40:46 +0800

    纯openai实现多轮对话

--- README.md
+++ README.md
@@ -10,8 +10,20 @@
     - 输出：输出的格式描述，以便后继模块自动解析模型的输出结果，比如（JSON、XML）
 大模型对 prompt 开头和结尾的内容更敏感
 
-### 1.1 第一个 prompt_test.py
+### 1.1 第一步 prompt_test.py
 将 openai 的prompt, messages等传入参数抽离成单独的一个函数 get_chat_completion
 
 ### 1.2 第二步 prompt_test_nlu.py
 自己实现一个NLU（语义理解）模块
+
+### 1.3 第三步 自己实现NLU和DST多轮对话 prompt_test_nlu.py
+- 步骤
+    - 调用NLU获得语义解析：调用了openai API，理解用户输入，约定好输出格式便于后续程序处理
+    - 调用DST更新多轮状态
+    - 根据状态检索DB，获得满足条件的候选产品
+    - 拼装prompt调用chatgpt：根据上面选出的字段组装prompt，让chatgpt生成自然语言
+    - 将当前用户输入和系统回复维护入chatgpt的session：多轮对话的上下文
+- 这里只是用chatgpt识别了用户意思和生成了筛选出的产品介绍。产品的筛选靠的是程序
+
+### 1.4 第四步 纯用openai API实现第三步的内容 prompt_test_nlu_openai.py
+- 这里用openai直接回答用户问题。用户意图理解nlu，状态跟踪dst和产品筛选这中间的多个步骤都是在大模型中完成
---
+++ prompt/prompt_test_nlu_openai.py
@@ -0,0 +1,54 @@
+########### Prompt 的典型构成 ##############
+# 角色：给 AI 定义一个最匹配任务的角色，比如：「你是一位软件工程师」「你是一位小学老师」
+# 指示：对任务进行描述
+# 上下文：给出与任务相关的其它背景信息（尤其在多轮交互中）
+# 例子：必要时给出举例，学术中称为 one-shot learning, few-shot learning 或 in-context learning；实践证明其对输出正确性有帮助
+# 输入：任务的输入信息；在提示词中明确的标识出输入
+# 输出：输出的格式描述，以便后继模块自动解析模型的输出结果，比如（JSON、XML）
+
+## !!!!!!!!!! 大模型对 prompt 开头和结尾的内容更敏感 !!!!!!!!!!
+###########################################
+
+
+import os
+from urllib import response
+import openai
+import json
+import copy
+# 加载 .env 到环境变量
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+# 配置 OpenAI 服务
+openai.api_key = os.getenv('OPENAI_API_KEY') # 设置 OpenAI 的 key
+openai.api_base = os.getenv('OPENAI_API_BASE') # 指定代理地址
+
+session = [
+    {
+        "role": "system",
+        "content": """
+你是一个手机流量套餐的客服代表，你叫小瓜。可以帮助用户选择最合适的流量套餐产品。可以选择的套餐包括：
+经济套餐，月费50元，10G流量；
+畅游套餐，月费180元，100G流量；
+无限套餐，月费300元，1000G流量；
+校园套餐，月费150元，200G流量，仅限在校生。
+"""
+    }
+]
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    session.append({"role": "user", "content": prompt})
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=session,
+        temperature=0,  # 模型输出的随机性，0 表示随机性最小
+    )
+    msg = response.choices[0].message["content"]
+    session.append({"role": "assistant", "content": msg})
+    return msg
+
+if __name__ == "__main__":
+    get_completion("有没有土豪套餐？")
+    get_completion("多少钱？")
+    get_completion("给我办一个")
+    print(json.dumps(session, indent=4, ensure_ascii=False))  # 用易读格式打印对话历史
