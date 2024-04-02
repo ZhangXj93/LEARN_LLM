8ab16b0990c3a1d4689ca656adadb66fbecd8dd1
XINJ_ZH <xjzhang93@163.com>
2024-04-02 13:21:16 +0800

    Create langgraph_visual.py

---
+++ langgraph/langgraph_visual.py
@@ -0,0 +1,15 @@
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import chat_agent_executor
+from langchain_core.messages import HumanMessage
+
+tools = [TavilySearchResults(max_results=1)]
+model = ChatOpenAI()
+
+app = chat_agent_executor.create_function_calling_executor(model, tools)
+
+# app.get_graph().print_ascii()
+
+from IPython.display import Image
+
+Image(app.get_graph().draw_png())
